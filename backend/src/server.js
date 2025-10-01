25b79d1f6c3aab96c32d32c3bf0cd01404b3130c
Kabir Sidhu <kxbir414@gmail.com>
2025-10-01 10:14:20 -0700

    completed basic sign in and sign up auth using webhooks from inngest and clerk

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,37 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { inngest, functions } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
-const app = express();  
+const app = express();
 
- app.get("/", (req, res) => {
+app.use(express.json());
+app.use(clerkMiddleware()) // we need this to check if user is authenticated or not
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/", (req, res) => {
     res.send("Hello World!");
-    });
+});
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-    connectDB();
-    });
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        if (ENV.NODE_ENV !== "production") {
+            app.listen(ENV.PORT, () => {
+                console.log(`Server is running on port ${ENV.PORT}`);
+            });
+        }
+
+    } catch (error) {
+        console.error("Error starting server:", error);
+
+    }
+};
+
+startServer();
+
+export default app; 
