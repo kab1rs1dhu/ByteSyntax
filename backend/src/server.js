30728725553a39c58360a94e35187b4a13ed187b
Kabir Sidhu <kxbir414@gmail.com>
2025-10-01 15:41:11 -0700

    finished setting up stream

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,19 @@ import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { inngest, functions } from "./config/inngest.js"; 
 import { serve } from "inngest/express";
+import chatRoutes from "./routes/chat.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(clerkMiddleware()) // we need this to check if user is authenticated or not
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes); 
 
 const startServer = async () => {
     try {
